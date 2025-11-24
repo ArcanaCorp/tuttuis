389a1d2aff60b644931545d05f5a40aab6d99b77
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-11-24 17:11:20 -0500

    fix:scan

--- src/featured/dashboard/services/assistence.services.js
+++ src/featured/dashboard/services/assistence.services.js
@@ -1,22 +1,23 @@
 import { REACT_API_URL } from "@/config.js";
 import Cookies from "js-cookie";
 
-const user = Cookies.get('tuttis_user');
 
 export const serviceRegisterAssitence = async (token, dia, hora) => {
-    try {
-        const response = await fetch(`${REACT_API_URL}/student/assistence`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': `Bearer ${user}`
-            },
-            body: JSON.stringify({token, dia, hora})
-        })
-        const data = await response.json()
-        if (!response.ok) throw new Error(data.message || response.statusText);
-            return data;
-    } catch (error) {
-        return { ok: false, message: `Error: ${error.message}`, error: error, status: 'SERVER_ERROR', code: 500 }
-    }
+    const user = Cookies.get('tuttis_user');
+    if (!user) return {ok: false, message: 'No se recibió el token', error: 'NOT_FOUND', status: 'NOT_FOUND', code: 404}
+        try {
+            const response = await fetch(`${REACT_API_URL}/student/assistence`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': `Bearer ${user}`
+                },
+                body: JSON.stringify({token, dia, hora})
+            })
+            const data = await response.json()
+            if (!response.ok) throw new Error(data.message || response.statusText);
+                return data;
+        } catch (error) {
+            return { ok: false, message: `Error: ${error.message}`, error: error, status: 'SERVER_ERROR', code: 500 }
+        }
 }
